ea4edd78c67a49377927774c8d7b58e50138734a
vinayakdhar <vinayakdhar003@gmail.com>
2024-10-23 21:51:29 +0530

    added image slider in blog and news page

--- main file/script.js
+++ main file/script.js
@@ -29,3 +29,48 @@ loginSignupLink.forEach(link => {
         formPopup.classList[link.id == "signup-link" ? 'add' : 'remove']("show-signup");
     });
 })
+
+
+// image slider 
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 0;
+let intervalId = null;
+
+
+// initialzeSlider();
+document.addEventListener('DOMContentLoaded', initialzeSlider);
+
+function initialzeSlider() {
+
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add('displaySlide');
+        intervalId = setInterval(nextSlide, 5000);
+    }
+
+    
+}
+
+function showSlide(index) {
+
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }
+    else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach(slide => {
+        slide.classList.remove('displaySlide');
+    });
+    slides[slideIndex].classList.add('displaySlide');
+}
+
+function prevSlide() {
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
